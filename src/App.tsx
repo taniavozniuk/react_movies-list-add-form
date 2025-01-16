38d4ccb3b6bdf56a558f0c851342aeb80e7728db
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-16 14:55:59 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,22 @@ import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
 export const App = () => {
+  function getImageById(imdbID: string): string | null {
+    return moviesFromServer.find(movie => movie.imdbId === imdbID) || null;
+  }
+
+  const initialPosts: Post[] = moviesFromServer.map(movie => ({
+    ...movie,
+    imdb: getImageById(movie.imdbId),
+  }));
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={initialPosts} />
       </div>
       <div className="sidebar">
-        <NewMovie  onAdd={(movie) => {}} />
+        <NewMovie onAdd={movie => {}} />
       </div>
     </div>
   );
